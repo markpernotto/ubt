e07013d213ec988ab15589940461682fb1226ac1
markpernotto <mark@pernotto.com>
2018-07-18 21:23:50 -0700

    Some explanations were in order

--- page-ubt-template.php
+++ page-ubt-template.php
@@ -2,11 +2,17 @@
 /**
  * Template Name: UBT Template
  */
+
+//These are used as variables within the Plugin template
 $city = urldecode(get_query_var( 'city' ));
 $state = urldecode(get_query_var( 'state' ));
 $city_state = $city.', '.$state;
+
+//So that we can still edit via CSS in theme Customizer
 get_header();
+
 global $wpdb;
+//This is gross - if there's more than one than page assigned this template then ¯\_(ツ)_/¯
 $result = $wpdb->get_results("SELECT template_content FROM ".$wpdb->prefix . 'ubt_templates');
 foreach($result as $ubt){
     $php_formatted_output = $ubt->template_content;
